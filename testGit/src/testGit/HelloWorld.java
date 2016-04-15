dcfc9a84810563847abb696a2ab9c8df823652f1
Slash <slash_2988@hotmail.com>
2016-04-15 10:44:28 +0800

    submit 20160415

--- testGit/src/testGit/HelloWorld.java
+++ testGit/src/testGit/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!!");
 		System.out.println("sumbit !!!");
+		
+		System.out.println("sumbit 20160415");
 	}
 
 }
